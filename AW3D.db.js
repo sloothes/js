3b3eabc5dfa0c12a20ff744675ebd071aa533c88
Jane Slooth <sloothes.com@gmail.com>
2019-07-08 22:44:45 +0300

    Update AW3D.db.js

--- AW3D.db.js
+++ AW3D.db.js
@@ -1,5 +1,7 @@
 //  AW3D.db.js
 
+(function(){
+
     const VERSION = 1;
     const DB_NAME = "AW3D";
 
@@ -22,3 +24,31 @@
     }).catch(function(err){
         console.error(err);
     });
+
+})();
+
+//  USER.db.js
+
+(function(){
+
+    const VERSION = 1;
+    const DB_NAME = "USER";
+
+    UserDB = new zango.Db( DB_NAME, VERSION, {
+
+        materials:  true,
+        textures:   true,
+        animations: true,
+
+    });
+
+    UserDB.open(function(err, database){
+        if (err) console.error(err);
+    }).then( function(){
+        debugMode && console.log( 
+        `Database ${UserDB.name} (v${UserDB.version}) ready for use.`);
+    }).catch(function(err){
+        console.error(err);
+    });
+
+})();
